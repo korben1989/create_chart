3995cccec3867a9d7ab9296cf4cab6f1b34ab310
Yurii Gorohov <yugorov11@gmail.com>
2023-09-23 13:58:00 +0300

    'delete url'

--- chart_app/forms.py
+++ chart_app/forms.py
@@ -1,17 +1,16 @@
 from django import forms
-from django.forms import models, NumberInput, FileInput, CharField, Select
+from django.forms import models, FileInput
 
 from .models import Uploadfiles
 
 
 class UploadfilesForm(models.ModelForm):
-    ex_mov_average = forms.IntegerField(widget=NumberInput(attrs={'placeholder': 'Enter EMA period',
-                                                                  'class': 'form-control'}))
     file = forms.FileField(widget=FileInput(attrs={'class': 'form-control'}))
-    # timeframe = forms.Select(attrs={'class': 'form-control'})
     class Meta:
         model = Uploadfiles
         fields = ('file', 'timeframe', 'ex_mov_average')
         widgets = {
             'timeframe': forms.Select(attrs={'class': 'form-control'}),
+            'ex_mov_average': forms.NumberInput(attrs={'class': 'form-control',
+                                                        'placeholder': 'Enter EMA period',}),
         }
